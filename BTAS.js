53761b13eff505fed1654b6f02c74d89cbad0ecf
Ripper-S <ripper-s@ripper-s.com>
2023-10-19 13:52:18 +0800

    update v1.8.9

--- BTAS.js
+++ BTAS.js
@@ -2,7 +2,7 @@
 // @name         BTAS
 // @namespace    https://github.com/Ripper-S/BTAS
 // @homepageURL  https://github.com/Ripper-S/BTAS
-// @version      1.8.8
+// @version      1.8.9
 // @description  Blue Team Assistance Script
 // @author       Barry Y Yang; Jack SA Chen; Xingyu X Zhou
 // @license      Apache-2.0
